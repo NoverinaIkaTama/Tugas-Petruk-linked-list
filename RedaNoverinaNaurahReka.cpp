f8694446f778841bf540b4cf743fe2f9c802ad90
redameiningtiyas <38450998+redameiningtiyas@users.noreply.github.com>
2018-04-30 23:31:47 +0700

    Update RedaNoverinaNaurahReka.cpp

--- RedaNoverinaNaurahReka.cpp
+++ RedaNoverinaNaurahReka.cpp
@@ -28,7 +28,24 @@ void init(){
     head = NULL;
     tail = NULL;
 }
+
 int isEmpty(){
  if(tail == NULL) return 1;
  else return 0;
 }
+
+void insertDepan(int databaru){
+  TNode *baru;
+  baru = new TNode;
+  baru->data =databaru;
+  baru->next =NULL;
+  if(isEmpty()==1){
+          head=tail=baru;
+          tail->next=NULL;
+     }
+  else {
+         baru->next = head;
+         head = baru;
+  }
+cout<<"Data masuk\n";
+} 
